5bc62fd89f6fbd1a04b9b374564aff2a56b3b397
BananaDragon <102924267+BananaDragon@users.noreply.github.com>
2024-04-05 23:26:04 -0400

    Add files via upload

--- tiles.c
+++ tiles.c
@@ -9,6 +9,8 @@
 /* include the tile map we are using */
 #include "space_map.h"
 
+#include "ufo.h"
+
 /* the width and height of the screen */
 #define WIDTH 240
 #define HEIGHT 160
@@ -24,6 +26,15 @@
 #define BG2_ENABLE 0x400
 #define BG3_ENABLE 0x800
 
+/* the address of the color palettes used for backgrounds and sprites */
+volatile unsigned short* sprite_palette = (volatile unsigned short*) 0x5000200;
+
+/* the memory location which stores sprite image data */
+volatile unsigned short* sprite_image_memory = (volatile unsigned short*) 0x6010000;
+
+volatile unsigned short* sprite_attribute_memory = (volatile unsigned short*) 0x7000000;
+
+
 /* the control registers for the four tile layers */
 volatile unsigned short* bg0_control = (volatile unsigned short*) 0x4000008;
 volatile unsigned short* bg1_control = (volatile unsigned short*) 0x400000a;
@@ -32,7 +43,11 @@ volatile unsigned short* bg3_control = (volatile unsigned short*) 0x400000e;
 
 /* palette is always 256 colors */
 #define PALETTE_SIZE 256
-
+#define NUM_SPRITES 128
+/* flags to set sprite handling in display control register */
+#define SPRITE_MAP_2D 0x0
+#define SPRITE_MAP_1D 0x40
+#define SPRITE_ENABLE 0x1000
 /* the display control pointer points to the gba graphics register */
 volatile unsigned long* display_control = (volatile unsigned long*) 0x4000000;
 
@@ -72,6 +87,265 @@ volatile short* bg3_y_scroll = (unsigned short*) 0x400001e;
 /* the scanline counter is a memory cell which is updated to indicate how
  * much of the screen has been drawn */
 volatile unsigned short* scanline_counter = (volatile unsigned short*) 0x4000006;
+
+#define DMA_ENABLE 0x80000000
+
+/* flags for the sizes to transfer, 16 or 32 bits */
+#define DMA_16 0x00000000
+#define DMA_32 0x04000000
+
+/* pointer to the DMA source location */
+volatile unsigned int* dma_source = (volatile unsigned int*) 0x40000D4;
+
+/* pointer to the DMA destination location */
+volatile unsigned int* dma_destination = (volatile unsigned int*) 0x40000D8;
+
+/* pointer to the DMA count/control */
+volatile unsigned int* dma_count = (volatile unsigned int*) 0x40000DC;
+
+/* copy data using DMA */
+void memcpy16_dma(unsigned short* dest, unsigned short* source, int amount) {
+    *dma_source = (unsigned int) source;
+    *dma_destination = (unsigned int) dest;
+    *dma_count = amount | DMA_16 | DMA_ENABLE;
+}
+
+/* setup the sprite image and palette */
+void setup_sprite_image() {
+    /* load the palette from the image into palette memory*/
+    memcpy16_dma((unsigned short*) sprite_palette, (unsigned short*) ufo_palette, PALETTE_SIZE);
+
+    /* load the image into sprite image memory */
+    memcpy16_dma((unsigned short*) sprite_image_memory, (unsigned short*) ufo_data, (ufo_width * ufo_height) / 2);
+}
+/* a sprite is a moveable image on the screen */
+struct Sprite {
+    unsigned short attribute0;
+    unsigned short attribute1;
+    unsigned short attribute2;
+    unsigned short attribute3;
+};
+
+struct Sprite sprites[NUM_SPRITES];
+int next_sprite_index = 0;
+
+/* the different sizes of sprites which are possible */
+enum SpriteSize {
+    SIZE_8_8,
+    SIZE_16_16,
+    SIZE_32_32,
+    SIZE_64_64,
+    SIZE_16_8,
+    SIZE_32_8,
+    SIZE_32_16,
+    SIZE_64_32,
+    SIZE_8_16,
+    SIZE_8_32,
+    SIZE_16_32,
+    SIZE_32_64
+};
+struct Sprite* sprite_init(int x, int y, enum SpriteSize size,
+         int tile_index, int priority) {
+
+    /* grab the next index */
+    int index = next_sprite_index++;
+
+    /* setup the bits used for each shape/size possible */
+    int size_bits, shape_bits;
+    switch (size) {
+        case SIZE_8_8:   size_bits = 0; shape_bits = 0; break;
+        case SIZE_16_16: size_bits = 1; shape_bits = 0; break;
+        case SIZE_32_32: size_bits = 2; shape_bits = 0; break;
+        case SIZE_64_64: size_bits = 3; shape_bits = 0; break;
+        case SIZE_16_8:  size_bits = 0; shape_bits = 1; break;
+        case SIZE_32_8:  size_bits = 1; shape_bits = 1; break;
+        case SIZE_32_16: size_bits = 2; shape_bits = 1; break;
+        case SIZE_64_32: size_bits = 3; shape_bits = 1; break;
+        case SIZE_8_16:  size_bits = 0; shape_bits = 2; break;
+        case SIZE_8_32:  size_bits = 1; shape_bits = 2; break;
+        case SIZE_16_32: size_bits = 2; shape_bits = 2; break;
+        case SIZE_32_64: size_bits = 3; shape_bits = 2; break;
+    }
+
+    /* set up the first attribute */
+    sprites[index].attribute0 = y     |         /* y coordinate */
+                            (0 << 8)  |         /* rendering mode */
+                            (0 << 10) |         /* gfx mode */
+                            (0 << 12) |         /* mosaic */
+                            (1 << 13) |         /* color mode, 0:16, 1:256 */
+                            (shape_bits << 14); /* shape */
+
+    /* set up the second attribute */
+    sprites[index].attribute1 = x     |         /* x coordinate */
+                            (0 << 9)  |         /* affine flag */
+                            (0 << 12) |         /* horizontal flip flag */
+                            (0 << 13) |         /* vertical flip flag */
+                            (size_bits << 14);  /* size */
+
+    /* setup the second attribute */
+    sprites[index].attribute2 = tile_index   |  /* tile index */
+                            (priority << 10) |  /* priority */
+                            (0 << 12);          /* palette bank (only 16 color)*/
+
+    /* return pointer to this sprite */
+    return &sprites[index];
+}
+
+/* set a sprite position */
+void sprite_position(struct Sprite* sprite, int x, int y) {
+    /* clear out the y coordinate */
+    sprite->attribute0 &= 0xff00;
+
+    /* set the new y coordinate */
+    sprite->attribute0 |= (y & 0xff);
+
+    /* clear out the x coordinate */
+    sprite->attribute1 &= 0xfe00;
+
+    /* set the new x coordinate */
+    sprite->attribute1 |= (x & 0x1ff);
+}
+/* move a sprite in a direction */
+void sprite_move(struct Sprite* sprite, int dx, int dy) {
+    /* get the current y coordinate */
+    int y = sprite->attribute0 & 0xff;
+
+    /* get the current x coordinate */
+    int x = sprite->attribute1 & 0x1ff;
+
+    /* move to the new location */
+    sprite_position(sprite, x + dx, y + dy);
+}
+/* change the tile offset of a sprite */
+void sprite_set_offset(struct Sprite* sprite, int offset) {
+    /* clear the old offset */
+    sprite->attribute2 &= 0xfc00;
+
+    /* apply the new one */
+    sprite->attribute2 |= (offset & 0x03ff);
+}
+/* update all of the spries on the screen */
+void sprite_update_all() {
+    /* copy them all over */
+    memcpy16_dma((unsigned short*) sprite_attribute_memory, (unsigned short*) sprites, NUM_SPRITES * 4);
+}
+struct UFO {
+    /* the actual sprite attribute info */
+    struct Sprite* sprite;
+
+    /* the x and y position */
+    int x, y;
+
+    /* which frame of the animation he is on */
+    int frame;
+
+    /* the number of frames to wait before flipping */
+    int animation_delay;
+
+    /* the animation counter counts how many frames until we flip */
+    int counter;
+};
+unsigned short tile_lookup(int x, int y, int xscroll, int yscroll,
+        const unsigned short* tilemap, int tilemap_w, int tilemap_h) {
+
+    /* adjust for the scroll */
+    x += xscroll;
+    y += yscroll;
+
+    /* convert from screen coordinates to tile coordinates */
+    x >>= 3;
+    y >>= 3;
+
+    /* account for wraparound */
+    while (x >= tilemap_w) {
+        x -= tilemap_w;
+    }
+    while (y >= tilemap_h) {
+        y -= tilemap_h;
+    }
+    while (x < 0) {
+        x += tilemap_w;
+    }
+    while (y < 0) {
+        y += tilemap_h;
+    }
+
+    /* the larger screen maps (bigger than 32x32) are made of multiple s  titched
+     * together - the offset is used for finding which screen block we a  re in
+     * for these cases */
+    int offset = 0;
+
+    /* if the width is 64, add 0x400 offset to get to tile maps on right   */
+    if (tilemap_w == 64 && x >= 32) {
+        x -= 32;
+        offset += 0x400;
+    }
+
+    /* if height is 64 and were down there */
+    if (tilemap_h == 64 && y >= 32) {
+        y -= 32;
+
+        /* if width is also 64 add 0x800, else just 0x400 */
+        if (tilemap_w == 64) {
+            offset += 0x800;
+        } else {
+            offset += 0x400;
+        }
+    }
+
+    /* find the index in this tile map */
+    int index = y * 32 + x;
+
+    /* return the tile */
+    return tilemap[index + offset];
+}
+/* initialize the ufo */
+void ufo_init(struct UFO* ufo) {
+    ufo->x = 112;
+    ufo->y = 72;
+    ufo->frame = 0;
+    ufo->counter = 0;
+    ufo->animation_delay = 6;
+    ufo->sprite = sprite_init(ufo->x, ufo->y, SIZE_16_16, ufo->frame, 0);
+}
+/* update the ufo */
+int ufo_update(struct UFO* ufo, int xscroll, int yscroll) {
+	unsigned short top_collider = tile_lookup(ufo->x + 8, ufo->y, xscroll, yscroll, space_map, space_map_width, space_map_height);
+	 
+	unsigned short bottom_collider = tile_lookup(ufo->x + 8, ufo->y + 15, xscroll, yscroll, space_map, space_map_width, space_map_height);
+	 
+	unsigned short right_collider = tile_lookup(ufo->x + 15, ufo->y + 8, xscroll, yscroll, space_map, space_map_width, space_map_height);
+	 
+	unsigned short left_collider = tile_lookup(ufo->x, ufo->y + 8, xscroll, yscroll, space_map, space_map_width, space_map_height);
+	 
+    ufo->counter++;
+	if (ufo->counter >= ufo->animation_delay) {
+        ufo->frame = ufo->frame + 8;
+        if (ufo->frame > 16) {
+            ufo->frame = 0;
+        }
+        sprite_set_offset(ufo->sprite, ufo->frame);
+        ufo->counter = 0;
+    }
+
+    sprite_position(ufo->sprite, ufo->x, ufo->y);
+	
+	if (top_collider == 2) {
+		return 1;
+	}
+	else if (bottom_collider == 2) {
+		return 1;
+	}
+	else if (right_collider == 2) {
+		return 1;
+	}
+	else if (left_collider == 2) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
 
 /* wait for the screen to be fully drawn so we can do something during vblank */
 void wait_vblank() {
@@ -137,6 +411,19 @@ void setup_background() {
         dest[i] = space_map[i];
     }
 }
+/* setup all sprites */
+void sprite_clear() {
+    /* clear the index counter */
+    next_sprite_index = 0;
+
+    /* move all sprites offscreen to hide them */
+    for(int i = 0; i < NUM_SPRITES; i++) {
+        sprites[i].attribute0 = HEIGHT;
+        sprites[i].attribute1 = WIDTH;
+    }
+}
+/* finds which tile a screen coordinate maps to, taking scroll into acco  unt */
+
 
 
 /* just kill time */
@@ -147,55 +434,72 @@ void delay(unsigned int amount) {
 /* the main function */
 int main() {
     /* we set the mode to mode 0 with bg0 on */
-    *display_control = MODE0 | BG0_ENABLE;
+	*display_control = MODE0 | BG0_ENABLE | SPRITE_ENABLE | SPRITE_MAP_1D;
 
     /* setup the background 0 */
     setup_background();
+	
+	setup_sprite_image();
+	
+	sprite_clear();
+	
+	struct UFO ufo;
+	ufo_init(&ufo);
 
     /* set initial scroll to 0 */
-    int xscroll = 0;
+    int xscroll = 48;
     int yscroll = 0;
 	
 	//set initial speed velocity counters to 0
 	int xcount = 0;
 	int ycount = 0;
-	
+	int collide = 0;
 	
 
     /* loop forever */
     while (1) {
-        /* scroll with the arrow keys */
-        if (button_pressed(BUTTON_DOWN)) {
-            if (ycount < 40) {
+		collide = ufo_update(&ufo, xscroll, yscroll);
+		/* scroll with the arrow keys */
+		if (button_pressed(BUTTON_DOWN)) {
+			if (ycount < 40) {
 				ycount++;
 			}
-        }
-        if (button_pressed(BUTTON_UP)) {
+		}
+		if (button_pressed(BUTTON_UP)) {
 			if (ycount > -40) {
 				ycount--;
 			}
-        }
-        if (button_pressed(BUTTON_RIGHT)) {
+		}
+		if (button_pressed(BUTTON_RIGHT)) {
 			if (xcount < 40) {
 				xcount++;
 			}
-        }
-        if (button_pressed(BUTTON_LEFT)) {
-            if (xcount > -40) {
+		}
+		if (button_pressed(BUTTON_LEFT)) {
+			if (xcount > -40) {
 				xcount--;
 			}
-        }
+		}
 		
 		xscroll+= (int) xcount/10;
 		yscroll+= (int) ycount/10;
+		
+		/* wait for vblank before scrolling */
+		wait_vblank();
+		if (collide == 1) {
+			collide = 0;
+			xscroll = 48;
+			yscroll = 0;
+			xcount = 0;
+			ycount = 0;
+		} 
+		*bg0_x_scroll = xscroll;
+		*bg0_y_scroll = yscroll;
+		
+		sprite_update_all();
 
-        /* wait for vblank before scrolling */
-        wait_vblank();
-        *bg0_x_scroll = xscroll;
-        *bg0_y_scroll = yscroll;
-
-        /* delay a lot */
-        delay(1000);
+		/* delay a lot */
+		delay(1000);
     }
 }
 
